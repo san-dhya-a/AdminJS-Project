f03555727dec1985fac76229f7692c1beb188729
MAREESWARI P <psandhya2503@gmail.com>
2026-04-13 17:13:45 +0530

    init

--- src/modules/page/page.service.ts
+++ src/modules/page/page.service.ts
@@ -3,16 +3,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Page } from '../../entities/page.entity.js';
 
-@Injectable()
+@Injectable()    //service class
 export class PageService {
   constructor(
     @InjectRepository(Page)
-    private pageRepository: Repository<Page>,
+    private pageRepository: Repository<Page>, // page table in database
   ) {}
 
   async findByType(type: string): Promise<any> {
     const page = await this.pageRepository.findOne({
-      where: { pageType: type as any },
+      where: { pageType: type as any }, // pageType is a column in the page table
     });
 
     if (!page) {
