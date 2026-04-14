59be2b68afac873ee1b3ca6e38cfaf1566217654
MAREESWARI P <psandhya2503@gmail.com>
2026-04-14 14:36:13 +0530

    init

--- src/admin/auth-provider.ts
+++ src/admin/auth-provider.ts
@@ -1,18 +1,28 @@
 import { DefaultAuthProvider } from 'adminjs';
-
+import bcrypt from 'bcryptjs';
 import componentLoader from './component-loader.js';
-import { DEFAULT_ADMIN } from './constants.js';
+import { User } from '../entities/user.entity.js';
 
-/**
- * Make sure to modify "authenticate" to be a proper authentication method
- */
 const provider = new DefaultAuthProvider({
   componentLoader,
   authenticate: async ({ email, password }) => {
-    if (email === DEFAULT_ADMIN.email) {
-      return { email };
+    // 1. Find user by email
+    const user = await User.findOneBy({ email });
+
+    // 2. If user exists, check password
+    if (user && user.password) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      
+      if (isPasswordValid) {
+        return {
+          email: user.email,
+          title: user.name || user.email,
+          avatarUrl: null, // You can add avatarUrl if available
+        };
+      }
     }
 
+    // 3. Fail authentication
     return null;
   },
 });
